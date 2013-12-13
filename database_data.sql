1c75cde2f18b07f0552131f77f7796005d49dd8a
Sam Horn <samuel.horn1@gmail.com>
2013-12-13 16:59:10 +1000

    Added customizable autocompleters

--- database_data.sql
+++ database_data.sql
@@ -1,24 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 4.0.5
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Oct 25, 2013 at 04:50 AM
--- Server version: 5.5.32-cll-lve
--- PHP Version: 5.3.17
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `traswi_2012vga`
---
 
 --
 -- Dumping data for table `2010_releases`
