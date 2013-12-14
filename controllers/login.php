ba9f85844b25f252ddf7b247b0249c48c07dc70d
Sam Horn <samuel.horn1@gmail.com>
2013-12-15 08:25:19 +1000

    #2 - several more files moved to MySQLi

--- controllers/login.php
+++ controllers/login.php
@@ -1,5 +1,6 @@
 <?php
-
+// TODO: replace the current library with something less hacked together
+// http://stackoverflow.com/questions/18674042/steam-api-authentication/18680478
 $result = SteamSignIn::validate();
 if (strlen($result) > 0) {
   $_SESSION['login'] = $result;
@@ -8,37 +9,49 @@ if (strlen($result) > 0) {
   $_SESSION['name'] = $info->personaname;
   $_SESSION['avatar'] = $info->avatar;
 
-  mysql_query("INSERT INTO `logins` (`ID`, `UserID`, `Timestamp`) VALUES (0, \"$result\", NOW())");
-  
-  $name = mysql_real_escape_string($info->personaname);
-  $avatar = mysql_real_escape_string(base64_encode(file_get_contents($info->avatar)));
-  $mysql = mysql_query("SELECT `SteamID` FROM `users` WHERE `SteamID` = \"$result\"");
-  if (mysql_num_rows($mysql) == 0) {
-    //echo "New user";
-    $query = "INSERT INTO `users` (`SteamID`, `Name`, `FirstLogin`, `LastLogin`, `Avatar`) VALUES (\"$result\", \"$name\", NOW(), NOW(), \"$avatar\")";
+  // Record the login
+  $query = "INSERT INTO `logins` (`UserID`, `Timestamp`) VALUES (?, NOW())";
+  $stmt = $mysql->prepare($query);
+  $stmt->bind_param('s', $result);
+  $stmt->execute();
+ 
+  $avatar = base64_encode(file_get_contents($info->avatar));
+
+  $query = "SELECT `SteamID` FROM `users` WHERE `SteamID` = ?";
+  $stmt = $mysql->prepare($query);
+  $stmt->bind_param('s', $result);
+  $stmt->execute();
+  $stmt->store_result();
+  if ($stmt->num_rows === 0) {
+    // New user
+    $query = "INSERT INTO `users` (`SteamID`, `Name`, `FirstLogin`,
+              `LastLogin`, `Avatar`) VALUES(?, ?, NOW(), NOW(), ?)";
+    $stmt = $mysql->prepare($query);
+    $stmt->bind_param('sss', $result, $info->personaname, $avatar);
   } else {
-    //echo "Returning user";
-    $query = "UPDATE `users` SET `Name` = \"$name\", `Avatar` = \"$avatar\", `LastLogin` = NOW() WHERE `SteamID` = \"$result\"";
+    // Returning user; update their name and avatar
+    $query = "UPDATE `users` SET `Name` = ?, `Avatar` = ?, `LastLogin` = NOW()
+              WHERE `SteamID` = ?";
+    $stmt = $mysql->prepare($query);
+    $stmt->bind_param('sss', $info->personaname, $avatar, $result);
   }
-  $result = mysql_query($query);
-  if (!$result) {
-    echo "Sorry, there was an error in processing your login.<br />".mysql_error();
-    die();
-  }
+  $stmt->execute();
   
-  // Thanks to http://stackoverflow.com/questions/5009685/encoding-cookies-so-they-cannot-be-spoofed-or-read-etc/5009903#5009903
+  // Thanks to http://stackoverflow.com/questions/5009685/#5009903
   $randomToken = hash('sha256',uniqid(mt_rand(), true).uniqid(mt_rand(), true));
   $randomToken .= ':'.hash_hmac('md5', $randomToken, $STEAM_API_KEY);
   setcookie("token", $randomToken, time()+60*60*24*30, "/", $DOMAIN);
   
-  $avatar = mysql_real_escape_string($info->avatar);
-  $query =  "REPLACE INTO `login_tokens` (`UserID`, `Name`, `Avatar`, `Token`, `Generated`, `Expires`) ";
-  $query .= "VALUES(\"{$_SESSION['login']}\", \"$name\", \"$avatar\", \"$randomToken\", NOW(), DATE_ADD(NOW(), INTERVAL 30 DAY))";
-  mysql_query($query);
-  
+  $query = "REPLACE INTO `login_tokens` (`UserID`, `Name`, `Avatar`, `Token`, 
+            `Generated`, `Expires`) VALUES(?, ?, ?, ?, NOW(),
+            DATE_ADD(NOW(), INTERVAL 30 DAY))";
+  $stmt = $mysql->prepare($query);
+  $stmt->bind_param('ssss', $result, $info->personaname, $info->avatar,
+    $randomToken);
+  $stmt->execute();
 }
 
+// Send them back where they came from
 $return = rtrim(implode("/", array_slice($SEGMENTS, 1)), "/");
-
 header("Location: http://$DOMAIN/$return");
 ?>
