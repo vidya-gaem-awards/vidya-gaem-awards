6e0f91d5fe931780858b9b40cd4b362780d36f49
Sam Horn <samuel.horn1@gmail.com>
2013-12-15 09:31:05 +1000

    Removed some duplication for viewing nominations.

--- controllers/nominations.php
+++ controllers/nominations.php
@@ -1,16 +1,4 @@
 <?php
 $tpl->set("title", "Nominations");
-if ($SEGMENTS[1] == "results") {
-  if (!canDo("nominations-view")) {
-    $PAGE = "403";
-  } else {
-    $CUSTOM_TEMPLATE = "results";
-    require_once("nominations-results.php");
-  }
-} else if (!canDo("nominees-view")) {
-  $PAGE = "403";
-} else {
-  $CUSTOM_TEMPLATE = "edit";
-  require_once("nominations-edit.php");
-}
-?>
+$CUSTOM_TEMPLATE = "manage";
+require_once("nominations-manage.php");
