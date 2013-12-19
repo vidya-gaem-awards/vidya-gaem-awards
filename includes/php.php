67569d6960e0dc92f65dd5508a7e384d72d4e1bc
Sam Horn <samuel.horn1@gmail.com>
2013-12-19 11:45:21 +1000

    Cleaning up php.php a bit

--- includes/php.php
+++ includes/php.php
@@ -16,14 +16,14 @@ session_start();
 // Constants
 $tpl->set("YEAR", $YEAR);
 
-// Database connections
+// Database connection
+$mysql = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_DB);
+$mysql->set_charset("utf8");
+
+// Backwards compatibility
 mysql_connect($DB_HOST, $DB_USER, $DB_PASS);
 mysql_select_db($DB_DB);
 mysql_query("SET NAMES utf8");
-
-// Forward compatibility
-$mysql = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_DB);
-$mysql->set_charset("utf8");
 
 // Initialise some default template variables
 $init = array("success", "error", "formSuccess", "formError");
@@ -41,22 +41,18 @@ if (!isset($_SESSION['login']) && isset($_COOKIE['token'])) {
   list($token, $hmac) = explode(':', $_COOKIE['token'], 2);
   $tokenValid = $hmac == hash_hmac('md5', $token, $STEAM_API_KEY);
   if ($tokenValid) {
-    $token = mysql_real_escape_string($_COOKIE['token']);
-    $query = "SELECT * FROM `login_tokens` WHERE `Token` = \"$token\"";
-    $result = mysql_query($query);
-    if (mysql_num_rows($result) === 1) {
-      $row = mysql_fetch_assoc($result);
-      if (strtotime($row['Expires']) > time()) {
-        $_SESSION['login'] = $row['UserID'];
-        $_SESSION['name'] = $row['Name'];
-        $_SESSION['avatar'] = $row['Avatar'];
-        $correct = true;
-      }
+    $query = "SELECT `UserID`, `Name`, `Avatar`, `Expires` FROM `login_tokens`
+              WHERE `Token` = ?";
+    $stmt = $mysql->prepare($query);
+    $stmt->bind_param('s', $_COOKIE['token']);
+    $stmt->execute();
+    $stmt->bind_result($userID, $name, $avatar, $expires);
+    if ($stmt->fetch() && strtotime($expires) > time()) {
+      $_SESSION['login'] = $userID;
+      $_SESSION['name'] = $name;
+      $_SESSION['avatar'] = $avatar;
+      $correct = true;
     }
-  }
-  
-  if (!$correct) {
-    //setcookie("token", "0", 1, "/", $DOMAIN);
   }
   
 }
@@ -71,14 +67,17 @@ if (isset($_SESSION['login'])) {
 	$tpl->set("steamID", convertSteamID($ID));
 	
 	// Find out which groups the user is in
-	$query = "SELECT `GroupName` FROM `user_groups` WHERE `UserID` = '$userID'";
-	$result = mysql_query($query);
-	while ($row = mysql_fetch_assoc($result)) {
-		$USER_GROUPS[] = $row['GroupName'];
-		if (substr($row['GroupName'], 0, 5) != "level") {
-      $USER_RIGHTS[] = $row['GroupName'];
+  $query = "SELECT `GroupName` FROM `user_groups` WHERE `UserID` = ?";
+  $stmt = $mysql->prepare($query);
+  $stmt->bind_param('s', $ID);
+  $stmt->execute();
+  $stmt->bind_result($groupName);
+  while ($stmt->fetch()) {
+    $USER_GROUPS[] = $groupName;
+    if (substr($groupName, 0, 5) != "level") {
+      $USER_RIGHTS[] = $groupName;
     }
-	}
+  }
 	
 	// Level 5s are assigned to levels 1-4
 	// Level 4s are assigned to levels 1-3
@@ -91,8 +90,8 @@ if (isset($_SESSION['login'])) {
 	
 	$query = "SELECT DISTINCT `CanDo` FROM `user_rights` WHERE `GroupName` IN ('";
 	$query .= implode("', '", $USER_GROUPS) . "')";
-	$result = mysql_query($query);
-	while ($row = mysql_fetch_array($result)) {
+	$result = $mysql->query($query);
+	while ($row = $result->fetch_assoc()) {
 		$USER_RIGHTS[] = $row['CanDo'];
 	}
 	
@@ -106,32 +105,11 @@ if (isset($_SESSION['login'])) {
 	$tpl->set("openIDurl", SteamSignIn::genUrl("http://$DOMAIN/login/$page"));
 	
   $ID = isset($_SERVER['HTTP_CF_CONNECTING_IP']) ? $_SERVER['HTTP_CF_CONNECTING_IP'] : $_SERVER['REMOTE_ADDR'];
-	
 	$userID = "";
 	
-}
+} 
 $tpl->set("loggedIn", $loggedIn);
-if (canDo("admin")) {
-	error_reporting(E_ALL);
-	
-	// Pretend to be the specified ID until further notice
-	if (isset($_GET['pretend'])) {
-		if (empty($_GET['pretend'])) {
-			unset($_SESSION['pretend']);
-		} else {
-			$_SESSION['pretend'] = $_GET['pretend'];
-		}
-	}
-	
-	if (isset($_SESSION['pretend'])) {
-		$ID = $_SESSION['pretend'];
-		$result = mysql_query("SELECT `Name` FROM `users` WHERE `SteamID` = \"$userID\"");
-		$row = mysql_fetch_array($result);
-		$tpl->set("displayName", $row['Name']);
-	}
 
-}
-$tpl->set("pretend", isset($_SESSION['pretend']));
 set_time_limit(60);
 
 if (!isset($_COOKIE['access']) || strlen($_COOKIE['access']) <= 10) {
@@ -140,7 +118,7 @@ if (!isset($_COOKIE['access']) || strlen($_COOKIE['access']) <= 10) {
   $uniqueID = $randomToken;
 	setcookie("access", $randomToken, time()+60*60*24*90, "/", $DOMAIN);
 } else {
-  $uniqueID = mysql_real_escape_string($_COOKIE['access']);
+  $uniqueID = $_COOKIE['access'];
 }
 
 
@@ -180,25 +158,8 @@ foreach ($NAVBAR_ITEMS as $filename => $value) {
 
 $tpl->set("navbar", $navbar);
 
-// Slash removal stuff
-if (get_magic_quotes_gpc()) {
-    array_walk_recursive($_POST, create_function('&$val', '$val = stripslashes($val);'));
-}
-
 // Page access stuff
 $page = $PAGE;
-$reqString = userInput($_SERVER['REQUEST_URI']);
-if (isset($_SERVER['HTTP_USER_AGENT'])) {
-  $userAgent = userInput($_SERVER['HTTP_USER_AGENT']);
-} else {
-  $userAgent = "";
-}
-$filename = userInput($_SERVER['SCRIPT_FILENAME']);
-if (isset($_SERVER['HTTP_REFERER'])) {
-	$refer = "'".userInput($_SERVER['HTTP_REFERER'])."'";
-} else {
-	$refer = "NULL";
-}
 
 $ip = isset($_SERVER['HTTP_CF_CONNECTING_IP']) ? $_SERVER['HTTP_CF_CONNECTING_IP'] : $_SERVER['REMOTE_ADDR'];
 $country = isset($_SERVER['HTTP_CF_IPCOUNTRY']) ? $_SERVER['HTTP_CF_IPCOUNTRY'] : "";
@@ -207,14 +168,16 @@ $tpl->set("page", $page);
 $tpl->set("logoutURL", rtrim(implode("/", $SEGMENTS), "/"));
 
 # Don't bother recording automatic page refreshes.
-if (substr($reqString, -11) != "autorefresh") {
-  $_unique = mysql_real_escape_string($uniqueID);
-
-	$query = "INSERT INTO `access` (`Timestamp`, `UniqueID`, `UserID`, `Page`, `RequestString`, `RequestMethod`, `IP`, `UserAgent`, `Filename`, `Refer`) ";
-	$query .= "VALUES (NOW(), '$_unique', '$userID', '$page', '$reqString', '{$_SERVER['REQUEST_METHOD']}', '$ip', ";
-	$query .= "'$userAgent', '$filename', $refer)";
-	mysql_query($query);
-	
+if (substr($_SERVER['REQUEST_URI'], -11) != "autorefresh") {
+	$query = "INSERT INTO `access` (`Timestamp`, `UniqueID`, `UserID`, `Page`, 
+            `RequestString`, `RequestMethod`, `IP`, `UserAgent`, `Filename`,
+            `Refer`) VALUES (NOW(), ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+  $stmt = $mysql->prepare($query);
+  $stmt->bind_param('sssssssss', $uniqueID, $userID, $page,
+    $_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD'], $ip,
+    $_SERVER['HTTP_USER_AGENT'], $_SERVER['SCRIPT_FILENAME'],
+    $_SERVER['HTTP_REFERER']);
+	$stmt->execute();
 }
 
 $tpl->set("true", true);
