eac6373acdb4396498a8a1d5e9a163c9b7307ff7
Sam Horn <samuel.horn1@gmail.com>
2016-02-03 10:54:38 +1000

    Make the random cookie more robust

--- public/index.php
+++ public/index.php
@@ -55,8 +55,19 @@ if ($user->canDo('view-debug-output')) {
 // Generate a random ID to keep in the cookie if one doesn't already exist.
 // We use this cookie as part of the voting identification process
 // (although it's similar to the remember me token, they serve different purposes)
-$randomID = $request->cookies->get('access');
-if ($randomID === null) {
+$randomIDCookie = $request->cookies->get('access');
+$randomIDSession = $session->get('access');
+
+if ($randomIDCookie && $randomIDSession) {
+    $randomID = $randomIDCookie;
+} elseif ($randomIDSession && !$randomIDCookie) {
+    // Bad practice, should be using Symfony's request class
+    setcookie('access', $randomIDSession, strtotime('+90 days'), '/', DOMAIN);
+    $randomID = $randomIDSession;
+} elseif ($randomIDCookie && !$randomIDSession) {
+    $session->set('access', $randomIDCookie);
+    $randomID = $randomIDCookie;
+} else {
     $factory = new \RandomLib\Factory;
     $generator = $factory->getLowStrengthGenerator();
     $randomID = hash('sha256', $generator->generate(64));
@@ -64,6 +75,7 @@ if ($randomID === null) {
 
     // Bad practice, should be using Symfony's request class
     setcookie('access', $randomID, strtotime('+90 days'), '/', DOMAIN);
+    $session->set('access', $randomID);
 }
 
 $votingCodeSession = $session->get('votingCode');
