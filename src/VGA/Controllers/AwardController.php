a4d735c88f8a1b0554479006c6637756589a9dda
Sam Horn <samuel.horn1@gmail.com>
2016-12-09 22:23:21 +1000

    Small improvements to the awards page

--- src/VGA/Controllers/AwardController.php
+++ src/VGA/Controllers/AwardController.php
@@ -153,7 +153,7 @@ class AwardController extends BaseController
         if ($nomination !== null) {
 
             if (!$award->areNominationsEnabled()) {
-                $response->setData(['error' => 'Nominations have been closed for this award.']);
+                $response->setData(['error' => 'Nominations are not currently open for this award.']);
                 $response->send();
                 return;
             }
