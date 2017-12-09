0466814c2ec52f5a9b997db1b330e5c7d961fbdc
Sam Horn <samuel.horn1@gmail.com>
2017-12-10 01:58:32 +1000

    Backported some referrer changes from 2017
    See commits 750c235 and 8d84805

--- controllers/referrers.php
+++ controllers/referrers.php
@@ -3,15 +3,24 @@ function startsWith($haystack, $needle) {
   return substr($haystack, 0, strlen($needle)) == $needle;
 }
 
+$days = $_GET['days'] ?? '7';
+if (!ctype_digit($days)) {
+    $days = 7;
+}
+
 $tpl->set("title", "Referrers");
 $query = "SELECT MAX(`Timestamp`) AS `Latest`,
 COUNT(*) AS `Count`, `Refer` FROM `access`
 WHERE `Refer` != \"\"
 AND `Refer` NOT LIKE \"%vidyaga__awards.com%\"
 AND `Refer` NOT LIKE \"https://%\"
-AND `Refer` != \"undefined\"
-AND `Timestamp` > DATE_SUB(NOW(), INTERVAL 5 DAY)
-GROUP BY `Refer`
+AND `Refer` != \"undefined\"";
+
+if ($days) {
+    $query .= "AND `Timestamp` > DATE_SUB(NOW(), INTERVAL $days DAY)";
+}
+
+$query .= "GROUP BY `Refer`
 HAVING `Count` >= 5
 ORDER BY `Count` DESC, `Latest` DESC";
 $result = $mysql->query($query);
@@ -47,9 +56,14 @@ while ($row = $result->fetch_assoc()) {
   if (startsWith($refer, "reddit.com")) { 
     $class = "error";
   }
+
+  if (strlen($row['Refer']) > 100) {
+      $row['Refer'] = substr($row['Refer'], 0, 100) . "...";
+  }
   
   $data[] = array("Count" => $row['Count'], "Latest" => $diffStr, "LatestAlt" => $row['Latest'], "Link" => $link, "LinkName" => $linkName, "Refer" => $row['Refer'], "Class" => $class);
 }
 
 $tpl->set("data", $data);
+$tpl->set("days", $days);
 ?>
