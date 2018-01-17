08d01a789f3221c821678937537654c4559d7ff3
Sam Horn <samuel.horn1@gmail.com>
2018-01-17 23:02:23 +1000

    Updated stream page for the marathon

--- src/AppBundle/Controller/ResultController.php
+++ src/AppBundle/Controller/ResultController.php
@@ -84,7 +84,7 @@ class ResultController extends Controller
 //                '10-null-without-voting-code' => 'NULL without code',
 //            ],
             [
-                '19-google' => 'Google',
+                '19-google' => 'Trident Gaming',
                 '15-facepunch' => 'Facepunch',
                 '18-facebook' => 'Facebook',
                 '12-twitter' => 'Twitter',
