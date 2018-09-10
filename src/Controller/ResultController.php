a2f7909a6d6a24892a22d951fa549bf120194bf7
Sam Horn <samuel.horn1@gmail.com>
2018-09-10 21:23:17 +1000

    Update the look of the detailed results page

--- src/Controller/ResultController.php
+++ src/Controller/ResultController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use App\Entity\Action;
+use App\Entity\ResultCache;
 use App\Service\AuditService;
 use App\Service\ConfigService;
 use Doctrine\ORM\EntityManagerInterface;
@@ -65,41 +66,27 @@ class ResultController extends Controller
         $results = [];
 
         $filters = [
-            [
-                '01-all' => 'No filtering',
-                '03-null' => 'No referrer',
-                '08-4chan-or-null-with-voting-code' => 'Certified 4chan',
-                '16-8chan' => '8chan',
-//                '02-voting-code' => 'Voting code',
-//                '04-4chan' => '4chan',
-//            ],
-//            [
-//                '05-4chan-and-voting-code' => '4chan with code',
-//                '06-4chan-without-voting-code' => '4chan without code',
-
-//                '07-4chan-or-null' => '4chan + NULL',
-            ],
-//            [
-//                '09-null-and-voting-code' => 'NULL with code',
-//                '10-null-without-voting-code' => 'NULL without code',
-//            ],
-            [
-                '19-google' => 'Google',
-                '15-facepunch' => 'Facepunch',
-                '18-facebook' => 'Facebook',
-                '12-twitter' => 'Twitter',
-            ],
-            [
-//                '17-twitch' => 'Twitch',
-                '11-reddit' => 'Reddit',
-                '21-kiwifarms' => 'Kiwifarms',
-//                '14-neogaf' => 'NeoGAF',
-//                '13-something-awful' => 'Something Awful',
-            ],
-//            [
-//                '20-yandex' => 'Yandex',
-
-//            ]
+            '01-all' => 'No filtering',
+//                '03-null' => 'No referrer',
+            '08-4chan-or-null-with-voting-code' => '4chan',
+            '19-google' => 'Google',
+            '16-8chan' => '8chan',
+//            '02-voting-code' => 'Voting code',
+//            '04-4chan' => '4chan',
+//            '05-4chan-and-voting-code' => '4chan with code',
+//            '06-4chan-without-voting-code' => '4chan without code',
+//            '07-4chan-or-null' => '4chan + NULL',
+//            '09-null-and-voting-code' => 'NULL with code',
+//            '10-null-without-voting-code' => 'NULL without code',
+            '15-facepunch' => 'Facepunch',
+            '18-facebook' => 'Facebook',
+            '12-twitter' => 'Twitter',
+            '17-twitch' => 'Twitch',
+            '11-reddit' => 'Reddit',
+            '21-kiwifarms' => 'Kiwifarms',
+            '14-neogaf' => 'NeoGAF',
+            '13-something-awful' => 'Something Awful',
+            '20-yandex' => 'Yandex',
         ];
 
         $nominees = [];
@@ -109,8 +96,15 @@ class ResultController extends Controller
                 $nominees[$award->getId()][$nominee->getShortName()] = $nominee;
             }
             foreach ($award->getResultCache() as $result) {
-                $results[$award->getId()][$result->getFilter()] = $result;
+                if (isset($filters[$result->getFilter()]) && $result->getVotes() >= 5) {
+                    $results[$award->getId()][$result->getFilter()] = $result;
+                }
             }
+
+            // Display the filter with the most votes first (this will invariably put No Filtering and 4chan on top)
+            uasort($results[$award->getId()], function (ResultCache $a, ResultCache $b) {
+                return $b->getVotes() <=> $a->getVotes();
+            });
 
             // If the result cache is empty, results haven't been generated yet.
             if ($award->getResultCache()->isEmpty()) {
