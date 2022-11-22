022a69e14c719e0085f699fdb49e2f2ee145dfb4
Sam Horn <samuel.horn1@gmail.com>
2022-11-22 19:28:01 +1000

    fix: allow all BaseUser fields to be nullable

--- src/Entity/BaseUser.php
+++ src/Entity/BaseUser.php
@@ -6,28 +6,28 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 abstract class BaseUser implements UserInterface
 {
-    protected string $ipAddress;
-    protected string $randomID;
+    protected ?string $ipAddress;
+    protected ?string $randomID;
     protected ?string $votingCode = null;
 
-    public function setIP(string $ipAddress): self
+    public function setIP(?string $ipAddress): self
     {
         $this->ipAddress = $ipAddress;
         return $this;
     }
 
-    public function getIP(): string
+    public function getIP(): ?string
     {
         return $this->ipAddress;
     }
 
-    public function setRandomID(string $randomID): self
+    public function setRandomID(?string $randomID): self
     {
         $this->randomID = $randomID;
         return $this;
     }
 
-    public function getRandomID(): string
+    public function getRandomID(): ?string
     {
         return $this->randomID;
     }
